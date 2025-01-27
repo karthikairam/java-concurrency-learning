9c9fb3aed7e37fb54f4c4f5457f3522bd9926b93
Karthikaiselvan <karthikairam@gmail.com>
2025-01-27 23:27:29 +0400

    Virtual Thread

---
+++ src/main/java/com/learning/virtual_threads/VirtualThreadValidation.java
@@ -0,0 +1,7 @@
+package com.learning.virtual_threads;
+
+public class VirtualThreadValidation {
+    public static void main(String[] args) {
+        //Project Settings has JDK 21 issue. Need to fix it before experimenting
+    }
+}
